1fdae4d61cc9974648e8348df20a5c3cce20f498
MadhanV13 <vmadhan321@gmail.com>
2025-05-15 11:02:46 +0530

    updated with login and register

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,32 @@
-// Project: Doozy - Simple Login Screen in Flutter
-
-import 'package:doozy/provider/loginprovider.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'package:test_todo/pages/login_page.dart';
+import 'package:test_todo/providers/login_provider.dart'; // Make sure this path is correct
+import 'package:test_todo/providers/task_provider.dart';
+import 'firebase_options.dart';
 
-import 'screen/register.dart'; // This imports the register screen
-
-
-void main() {
-  runApp(const DoozyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
-class DoozyApp extends StatelessWidget {
-  const DoozyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers:[
+      providers: [
         ChangeNotifierProvider(create: (_) => LoginProvider()),
-        //ChangeNotifierProvider(create: (_) => TaskProvider()),
+        ChangeNotifierProvider(create: (_) => TaskProvider()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Doozy Login',
-        theme: ThemeData(
-          primarySwatch: Colors.indigo,
-        ),
         home: const LoginPage(),
-      ),
-    );
-  }
-}
-
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  void _login() {
-    if (_formKey.currentState!.validate()) {
-      String username = _usernameController.text;
-      String password = _passwordController.text;
-
-      // TODO: Replace this with real authentication logic
-      print('Username: $username');
-      print('Password: $password');
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Welcome to Doozy! Logging in...')),
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Doozy Login')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: _usernameController,
-                decoration: const InputDecoration(labelText: 'Username'),
-                validator: (value) =>
-                value == null || value.isEmpty ? 'Enter your username' : null,
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(labelText: 'Password'),
-                validator: (value) =>
-                value == null || value.isEmpty ? 'Enter your password' : null,
-              ),
-              const SizedBox(height: 32),
-              ElevatedButton(
-                onPressed: _login,
-                child: const Text('Login'),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const RegisterPage()),
-                  );
-                },
-                child: const Text("Don't have an account? Register"),
-              ),
-            ],
-
-          ),
-        ),
       ),
     );
   }
--- lib/provider/login_provider.dart
+++ lib/provider/login_provider.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-import '../ult/variables.dart';
+import '../ult/constants.dart';
 
 class LoginProvider with ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
